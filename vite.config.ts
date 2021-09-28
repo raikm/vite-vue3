d629f14a7b5e1850bf776a1ebe570be9c95f11c7
Raik <mail@raik-mueller.com>
2021-09-28 11:42:54 +0200

    fix: updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from "path"
-import { changePackageVersion } from "./build/plugins"
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src")
-    }
+      "@": resolve(__dirname, "./src"),
+    },
   },
   plugins: [
-    changePackageVersion(),
     vue({
-      refTransform: [/src/]
-    })
-  ]
-})
+      refTransform: [/src/],
+    }),
+  ],
+});
